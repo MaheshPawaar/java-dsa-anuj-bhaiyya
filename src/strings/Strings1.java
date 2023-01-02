46c71f8805f7e5ea9a307a903ded6f4e51fee6ec
Mahesh Pawar <pawarmahesh723@gmail.com>
2023-01-02 11:47:26 +0530

    Added String1 Q. solutions

--- src/strings/Strings1.java
+++ src/strings/Strings1.java
@@ -17,10 +17,27 @@ public class Strings1 {
 //        String s2 = "adacc";
 //        System.out.println(isAnagram(s1, s2));
 
-        String s = "I am the boss";
-        String ans = reverseWordsInString(s);
-        System.out.println(ans);
+//        String s = "I am the boss";
+//        String ans = reverseWordsInString(s);
+//        System.out.println(ans);
+
+//        String str1 = "abc";
+//        String str2 = "baacd";
+//        System.out.println(isSubsequence(str1, str2));
+
+//        String str = "applejuice";
+//        int index = firstAppearanceOfRepeatedChar(str);
+//        if (index == -1)
+//        {
+//            System.out.printf("Either all characters are "
+//                    + "distinct or string is empty");
+//        }else {
+//            System.out.println("Char is: "+str.charAt(index));
+
+        String str = "I am the boss";
+        System.out.println("Length is: " + lengthOfLastWord(str));
     }
+
 
     // Check if two strings are anagram of each other
     public static boolean isAnagram(String str1, String str2) {
@@ -49,7 +66,7 @@ public class Strings1 {
     }
 
     // Reverse the words in a given string
-     static String reverseWordsInString(String s) {
+    static String reverseWordsInString(String s) {
         // Approaches
         // 1. Using split() method
         // 2. Using Stack
@@ -84,5 +101,72 @@ public class Strings1 {
             i++;
             j--;
         }
+    }
+
+    // Check if a String is a subsequence of another string
+    static boolean isSubsequence(String str1, String str2) {
+        //Approach
+        // 1. Using Recursion
+        // 2. Using Two Pointers (Iterative)
+        int m = str1.length(), n = str2.length();
+        int i = 0, j = 0;
+
+        while (i < m && j < n) {
+            if (str1.charAt(i) == str2.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        // If i reaches till m (str1.length()), then it is subsequence else not
+        return i == m;
+    }
+
+    // Find the Leftmost character that repeats in a string
+    static int firstAppearanceOfRepeatedChar(String str) {
+        // Approach
+        // 1. By tracking first occurrence
+        // Initialize leftmost index array of every char as -1
+        int[] firstIndex = new int[256];
+        for (int i = 0; i < 256; i++) {
+            firstIndex[i] = -1;
+        }
+        // Traverse from left and update result
+        // if we see a repeating character whose
+        // first index is smaller than current
+        // result.
+        int res = Integer.MAX_VALUE;
+        char[] c = str.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (firstIndex[c[i]] == -1) {
+                firstIndex[c[i]] = i;
+            } else {
+                res = Math.min(res, firstIndex[c[i]]);
+            }
+        }
+
+        if (res == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return res;
+        // 2. By reverse Traversal
+    }
+
+    // Find the length of the last word in a given String
+    static int lengthOfLastWord(String str) {
+        // Approach
+        // 1. Iterating string from index 0
+        int length = 0;
+        String s = str.trim();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                length = 0;
+            } else {
+                length++;
+            }
+        }
+        return length;
+        // 2. Iterate the string from the last index
     }
 }
